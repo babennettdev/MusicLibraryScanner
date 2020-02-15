3dd2ec87d9bfef1698a046cc530f2b43e06a923c
Chris <christopherwboggs@gmail.com>
2020-02-15 17:34:26 -0500

    Fixing name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,33 +2,26 @@ package main.java;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.Properties;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         try {
-            URL url = new URL("https://api.discogs.com/database/search?q=810599020514");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
-            conn.setRequestProperty("Authorization", "Discogs key=@KEY, secret=@SECRET");
-
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("HTTP error code : " + conn.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-            String output = br.readLine();
-            System.out.println(output);
-
-            conn.disconnect();
+            InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties");
+            Properties props = new Properties();
+            props.load(input);
+            System.out.println("input" +  props);
+            RestController restController = new RestController();
+            String discogsResponse = restController.getDiscogsInfo(props.getProperty("discogsKey"), props.getProperty("discogsSecret"));
+            System.out.println(discogsResponse);
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
--- src/main/java/RestController.java
+++ src/main/java/RestController.java
@@ -1,4 +1,46 @@
 package main.java;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
+
 public class RestController {
-}
+
+    public String getDiscogsInfo(String discogsKey, String discogsSecret){
+        String output = "";
+        try {
+            InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties");
+            Properties props = new Properties();
+            props.load(input);
+            System.out.println("input" +  props);
+            URL url = new URL("https://api.discogs.com/database/search?q=810599020514");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("Authorization", "Discogs key=" + discogsKey + ", secret=" + discogsSecret );
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("HTTP error code : " + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            output = br.readLine();
+
+
+            conn.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output;
+
+    }
+    }
+
