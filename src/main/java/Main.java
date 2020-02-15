2a2e94b7575ec7d42f9f9ba8435adc48d07c227a
Chris <christopherwboggs@gmail.com>
2020-02-14 19:38:46 -0500

    Hitting rest service, need to add keys

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,41 @@
-package Main.Java;
+package main.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        System.out.println("Hello World!");
+        try {
+            URL url = new URL("https://api.discogs.com/database/search?q=810599020514");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("Authorization", "Discogs key=@KEY, secret=@SECRET");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("HTTP error code : " + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output = br.readLine();
+            System.out.println(output);
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
+
